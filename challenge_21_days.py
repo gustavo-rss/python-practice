4a8e46249442b497ae915eb9d0608d5281081254
Gustavo Ramos S. Silva <58111532+gustavo-rss@users.noreply.github.com>
2024-01-11 21:51:57 +0000

    Challenge 1 - Even or odd

---
+++ challenge_21_days.py
@@ -0,0 +1,8 @@
+
+# Challenge day 1 - Even or odd number 
+number = int(input('Input a number: '))
+
+if number % 2 == 0:
+    print(str(number) + " is even")
+else:
+   print(str(number) + " is odd")
